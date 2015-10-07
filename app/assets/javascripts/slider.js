05c566897f6d0a0f37811cfec6421005fa56c1aa
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-10-07 12:28:04 -0500

    Add slider

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,53 @@
+$(function() {
+  var SliderModule = (function() {
+    var pb = {};
+    pb.el = $('#slider');
+    pb.items = {
+      panel: pb.el.find('li')
+    }
+
+    var SliderInterval,
+      currentSlider = 0,
+      nextSlider = 1,
+      lengthSlider = pb.items.panel.length;
+
+    pb.init = function(settings) {
+      this.settings = settings || {duration: 8000} 
+      var output = '';
+
+      SliderInit();
+
+      for(var i = 0; i < lengthSlider; i++) {
+        if (i == 0) {
+          output += '<li class="active"></li>';
+        } else {
+          output += '<li></li>';
+        }
+      }
+    }
+
+    var SliderInit = function() {
+      SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
+    }
+
+    pb.startSlider = function() {
+      var panels = pb.items.panel,
+        controls = $('#slider-controls li');
+
+      if (nextSlider >= lengthSlider) {
+        nextSlider = 0;
+        currentSlider = lengthSlider-1;
+      }
+
+      controls.removeClass('active').eq(nextSlider).addClass('active');
+      panels.eq(currentSlider).fadeOut('slow');
+      panels.eq(nextSlider).fadeIn('slow');
+
+      currentSlider = nextSlider;
+      nextSlider += 1;
+    }
+
+    return pb;
+  }());
+  SliderModule.init({duration: 10000});
+});
