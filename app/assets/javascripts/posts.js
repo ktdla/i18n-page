25c524e2e33a2297710baeba8015e6f82a58ecf8
Moisés Ñañez <moisesnandres@gmail.com>
2015-10-08 18:19:30 -0500

    Try 2 ways to filter

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,13 +1,22 @@
+//$(".blog article").each(function() {
+//  var category = $("a").text();
+//  alert(category);
+//})
+
+//for (i=0; i<$(".blog-1").length; i++) {
+  //var category = $(".category").text();
+  //alert(category);
+//}
 
 $( "select" ).click(function(){
   var value = $( "option:selected" ).text();
-  var category = $( ".category" ).text();
+  alert(value);
   if (value == "Noticias") {
     $ (".blog-1").hide();
     $("#Noticias").show();
   }
   else if (value == "Recetas") {
-    $ (".blog-1").hide();
+    $ (".blog-1").hide()
   }
   else {
     $ (".blog-1").show();
