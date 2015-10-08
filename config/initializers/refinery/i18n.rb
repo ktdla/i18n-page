49540d3fc9965179526704f2f9095f928b642cc8
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-10-08 11:11:24 -0500

    Configure i18n initializer

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
-  # config.default_locale = :en
+  config.default_locale = :es
 
-  # config.current_locale = :en
+  config.current_locale = :es
 
-  # config.default_frontend_locale = :en
+  config.default_frontend_locale = :es
 
   config.frontend_locales = [:en, :pt, :es]
 
