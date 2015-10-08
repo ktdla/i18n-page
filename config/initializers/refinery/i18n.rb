772556744c764619cfde115c4c31cea340b5c308
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-10-08 18:37:24 -0500

    Add view for each blog

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,13 +1,14 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
+  config.enable = true
   config.default_locale = :es
 
   config.current_locale = :es
 
   config.default_frontend_locale = :es
 
-  config.frontend_locales = [:en, :pt, :es]
+  config.frontend_locales = [:es, :en, :pt]
 
-  config.locales = {:en=>"English", :pt=>"Português", :es=>"Español"}
+  config.locales = {:es=>"Español", :en=>"English", :pt=>"Português"}
 end
