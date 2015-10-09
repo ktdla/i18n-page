6e47c58dd98d9faea149dd9b1e3ac180de75bcdc
Moisés Ñañez <moisesnandres@gmail.com>
2015-10-08 19:34:02 -0500

    Try another way

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,18 @@
-//$(".blog article").each(function() {
-//  var category = $("a").text();
-//  alert(category);
+//$(".blog article").each(function(index) {
+//  var category = $(".category").text();
+//  console.log(category);
 //})
 
-//for (i=0; i<$(".blog-1").length; i++) {
-  //var category = $(".category").text();
-  //alert(category);
-//}
+
+//var category = $("article").find(".category").text();
+//console.log(category);
+
 
 $( "select" ).click(function(){
+  for (i=0; i<($(".blog-1").length); i++) {
+    var category = $(".blog-1 .category").text();
+    console.log(category);
+  }
   var value = $( "option:selected" ).text();
   alert(value);
   if (value == "Noticias") {
@@ -17,6 +21,7 @@ $( "select" ).click(function(){
   }
   else if (value == "Recetas") {
     $ (".blog-1").hide()
+    $("Recetas").show();
   }
   else {
     $ (".blog-1").show();
