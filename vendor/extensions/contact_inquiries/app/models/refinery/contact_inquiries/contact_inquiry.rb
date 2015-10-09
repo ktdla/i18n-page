a3a8552389d4c0032d02ac23a1e8764d976d320e
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-10-08 20:53:51 -0500

    Validate field to contact form

--- vendor/extensions/contact_inquiries/app/models/refinery/contact_inquiries/contact_inquiry.rb
+++ vendor/extensions/contact_inquiries/app/models/refinery/contact_inquiries/contact_inquiry.rb
@@ -9,6 +9,7 @@ module Refinery
       # Add some validation here if you want to validate the user's input
       # We have validated the first string field for you.
       validates :name, :presence => true
+      validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     end
   end
 end
