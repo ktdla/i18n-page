3728bd0a74faa62c4ffff44dc9b2c15eefff4c3f
Moisés Ñañez <moisesnandres@gmail.com>
2015-10-08 20:19:30 -0500

    Add class to a

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,29 +1,27 @@
-//$(".blog article").each(function(index) {
-//  var category = $(".category").text();
-//  console.log(category);
-//})
+$(".blog-1").each(function() {
+  $(".category a").each(function(){
+    var category = $(this).text();
+    console.log(category);
+    console.log("1");
+  })
+  $(this).addClass(category);
+})
 
 
 //var category = $("article").find(".category").text();
 //console.log(category);
 
-
-$( "select" ).click(function(){
-  for (i=0; i<($(".blog-1").length); i++) {
-    var category = $(".blog-1 .category").text();
+/*for (i=0; i<($(".blog-1").length); i++) {
+    var category = $(".blog-1 .category a").html();
+    $(".blog-1").addClass(category);
+    alert(category);
     console.log(category);
-  }
-  var value = $( "option:selected" ).text();
-  alert(value);
-  if (value == "Noticias") {
-    $ (".blog-1").hide();
-    $("#Noticias").show();
-  }
-  else if (value == "Recetas") {
-    $ (".blog-1").hide()
-    $("Recetas").show();
-  }
-  else {
-    $ (".blog-1").show();
-  }
+}*/
+
+/*
+$( "select" ).click(function(){
+    console.log(category);
+  //var value = $( "option:selected" ).text();
+  //alert(value);
 });
+*/
